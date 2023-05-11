bb55e664e9e7df68223eefefc3ee1c800f264ce7
RomaLavrinenko <73121041+RomaLavrinenko@users.noreply.github.com>
2023-05-11 20:46:03 +0300

    Add files via upload

---
+++ lesson1/script.js
@@ -0,0 +1,18 @@
+let city = 'Київ';
+city = 'Львів';
+let address = city;
+alert(address);
+
+let n = 5;
+let result = Math.pow(n, 3);
+console.log(result);
+
+let pen = 4;
+let pricePen = '5.25 грн';
+let pencil = 6;
+let pricePencil = '3.50 грн';
+
+let sumPen = pen * parseFloat(pricePen);
+let sumPencil = pencil * parseFloat(pricePencil);
+
+document.write(sumPen + sumPencil);
